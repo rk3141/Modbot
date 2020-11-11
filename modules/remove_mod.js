9581aa5694c28be2a5fe269c962a701e4af9b914
Rishit Khandelwal <rishitkh@gmail.com>
2020-11-11 14:06:11 +0530

    I will do the module later

--- modules/remove_mod.js
+++
@@ -1,7 +0,0 @@
-exports.rmmod = (msg) => {
-    let assignee = msg.mentions.users.first();
-    if (!assignee) { msg.channel.send("Did ya mention someone?") ;return }
-    let member = msg.guild.member(assignee);
-    if (!member) {return}
-    member.roles.remove(msg.guild.roles.cache.find(role => role.name == "Mod"))
-}
